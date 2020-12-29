e764f1e43753d2f45d5b8395b39189a77f27132e
yeonheelee <mrncd1@gmail.com>
2020-12-29 18:10:46 +0900

    Initial Commit

---
+++ bindingex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bindingex
+//
+//  Created by 이연희 on 12/29/20.
+//  Copyright © 2020 이연희. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bindingexTests/bindingexTests.swift
@@ -0,0 +1,34 @@
+//
+//  bindingexTests.swift
+//  bindingexTests
+//
+//  Created by 이연희 on 12/29/20.
+//  Copyright © 2020 이연희. All rights reserved.
+//
+
+import XCTest
+@testable import bindingex
+
+class bindingexTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
